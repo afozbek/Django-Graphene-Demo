7be4e4f86536f2e556f9198c54125912d51f744f
afozbek <furkanozbek1995@gmail.com>
2019-04-02 17:49:08 +0300

    ınitial setup

---
+++ django_graphql_movies/movies/schema.py
@@ -0,0 +1,19 @@
+import graphene
+from graphene_django.types import DjangoObjectType, ObjectType
+from django_graphql_movies.movies.models import Actor, Movie
+
+# Create a Graphql type for the actor model
+class ActorType(DjangoObjectType):
+    class Meta:
+        model = Actor
+
+
+# Create a Graphql type for the movie model
+class MovieType(DjangoObjectType):
+    class Meta:
+        model = Movie
+
+
+class Query(ObjectType):
+    actor = graphene.Field(ActorType, id=graphene.Int())
+    movie = graphene.Field(MovieType, id=graphene.Int())
