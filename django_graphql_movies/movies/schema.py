50a146c6eca0787f28305f19ea47a71e25a0110b
afozbek <furkanozbek1995@gmail.com>
2019-04-02 18:21:24 +0300

    createActor & updateActor mutation

--- django_graphql_movies/movies/schema.py
+++ django_graphql_movies/movies/schema.py
@@ -14,9 +14,57 @@ class MovieType(DjangoObjectType):
         model = Movie
 
 
+# Create Input Object Types
+class ActorInput(graphene.InputObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+
+
+class MovieInput(graphene.InputObjectType):
+    id = graphene.ID()
+    title = graphene.String()
+    actors = graphene.List(ActorInput)
+    year = graphene.Int()
+
+
+# Create mutations for actorss
+class CreateActor(graphene.Mutation):
+    class Arguments:
+        input = ActorInput(required=True)
+
+    ok = graphene.Boolean()
+    actor = graphene.Field(ActorType)
+
+    @staticmethod
+    def mutate(root, info, input=None):
+        ok = True
+        actor_instance = Actor(name=input.name)
+        actor_instance.save()
+        return CreateActor(ok=ok, actor=actor_instance)
+
+
+class UpdateActor(graphene.Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        input = ActorInput(required=True)
+
+    ok = graphene.Boolean()
+    actor = graphene.Field(ActorType)
+
+    @staticmethod
+    def mutate(root, info, id, input=None):
+        ok = False
+        actor_intance = Actor.objects.get(pk=id)
+        if actor_intance:
+            ok = True
+            actor_intance.name = input.name
+            actor_intance.save()
+            return UpdateActor(ok=ok, actor=actor_intance)
+
+        return UpdateActor(ok=ok, actor=None)
+
+
 # query of the models
-
-
 class Query(ObjectType):
     actor = graphene.Field(ActorType, id=graphene.Int())
     movie = graphene.Field(MovieType, id=graphene.Int())
@@ -24,7 +72,6 @@ class Query(ObjectType):
     movies = graphene.List(MovieType)
 
     # add resolvers
-
     def resolve_actor(self, info, **kwargs):
         id = kwargs.get("id")
 
