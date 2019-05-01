99ae050d306d63631f061bad47cfa28cba183315
afozbek <furkanozbek1995@gmail.com>
2019-05-01 21:21:18 +0300

    changing main schema

--- django_graphql_movies/schema.py
+++ django_graphql_movies/schema.py
@@ -1,14 +1,17 @@
-import graphene  
-import django_graphql_movies.movies.schema
+import graphene
+import movies_.schema as Schema
 
-class Query(django_graphql_movies.movies.schema.Query, graphene.ObjectType):  
+
+class Query(Schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
 
-class Mutation(django_graphql_movies.movies.schema.Mutation, graphene.ObjectType):  
+
+class Mutation(Schema.Mutation, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
 
-schema = graphene.Schema(query=Query, mutation=Mutation) 
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
